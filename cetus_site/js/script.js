c27b2d29e0792a83f22a29d3dca408ad4f92066d
johnsanc <sanchjoh@oregonstate.edu>
2018-02-05 23:38:22 -0800

    Added CSS and more content for site.  Needs to fix adding HTML elements to chatbox.

--- cetus_site/js/script.js
+++ cetus_site/js/script.js
@@ -1,6 +1,7 @@
 const $input = $('#chatInput');
 const $submitButton = $('#submitButton');
 const synth = window.speechSynthesis;
+const chatBox = $('#chatbox');
 
 let voices = synth.getVoices();
 let voice;
@@ -11,18 +12,33 @@ $(voices).each(function(index) {
     }
 });
 
-
-
+// Greys out submit button if no input found
 if($input.val() === "") {
     $submitButton.prop('disabled', true);
 } else {
     $submitButton.prop('disabled', false);
 }
 
+// Removes text from input field on reload/load
+$(window).unload = function(event) {
+    $input.val() = "";
+};
+
 $submitButton.on('click', function(event) {
+    // Don't reload the page on submit
     event.preventDefault();
     
+    // If we have some input
+    if($input.val() !== "") {
+        $submitButton.prop('disabled', false);
+    }
 
+    // Add the text to the chat box
+    let htmlString = '<p class="user">';
+    htmlString += $input.val() + '</p>';
+    chatBox.append(htmlString);
+
+    // Make utterance
     let utterance = new SpeechSynthesisUtterance($input.val());
     utterance.voice = voice;
     utterance.pitch = '1';
