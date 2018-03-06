8b85eb27813bd732c957d419f1ba9103b95b95f2
johnsanc <sanchjoh@oregonstate.edu>
2018-03-05 19:43:37 -0800

    Site has been implemented as a React application.  Only thing left to finish implementing is the speech-to-text feature.

--- site_v2/src/App.js
+++ site_v2/src/App.js
@@ -3,10 +3,17 @@ import './App.css';
 import axios from 'axios';
 
 function ChatMessage(props) {
-  // return (
-  //
-  // );
+  if(props.currentState.responses.length > 0) {
+    return (
+      <div>
+        <p className="message bot">{props.currentState.userInput}</p>
+        <p className="message user">{props.currentState.responses.slice(-1)[0].botResponse}</p>
+      </div>
+    );
+  }
 }
+
+let id = 0;
 
 class App extends Component {
   constructor () {
@@ -25,7 +32,7 @@ class App extends Component {
   }
 
   handleSubmit (e) {
-    axios.get('http://138.68.45.183:3001/cetus', {
+    axios.get('http://184.105.3.121:3001/cetus', {
       params: {
         userText: this.state.userInput
       }
@@ -40,12 +47,13 @@ class App extends Component {
       // });
 
       let responses = this.state.responses;
-      console.log('Responses before:', responses);
+      // console.log('Responses before:', responses);
       responses.push({
         userMessage: this.state.userInput,
-        botResponse: response.data
+        botResponse: response.data,
+        key: id
       });
-      console.log('Responses after:', responses);
+      // console.log('Responses after:', responses);
       this.setState ({
 
           responses: responses
@@ -55,15 +63,17 @@ class App extends Component {
           // })
 
       });
+      id += 1;
     })
     .catch(function (error) {
       console.log(error);
+      alert("Message could not be sent, please try again.");
     });
     // console.log(this.state.userInput);
   }
 
   render() {
-    console.log("this.state", this.state);
+    // console.log("this.state", this.state);
     return (
       <div className="container">
           <header>
@@ -78,11 +88,15 @@ class App extends Component {
 
               <p className="message bot">Hey, lets talk!</p>
               <p className="message user">Hello!</p>
+              {this.state.responses.map(function(message, index){
+                  return (
+                      <ChatMessage currentState={this.state} key={id} />
+                  );
+              }.bind(this))}
 
           </div>
           <div className="chatSubmit">
               <input type="text" id="chatInput" onChange={(e) => this.handleUserInput(e)} />
-              <button type="submit" id="submitVoice"></button>
               <button type="submit" id="submitButton" onClick={(e) => this.handleSubmit(e)}>Submit</button>
           </div>
           <footer>
