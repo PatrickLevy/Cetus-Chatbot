17f3eb4d1114316f1864116f9070af9942ce57f3
Patrick Levy <mrpatricklevy@gmail.com>
2018-03-14 22:17:08 -0500

    localhost

--- site_v2/src/App.js
+++ site_v2/src/App.js
@@ -99,7 +99,7 @@ class App extends Component {
     this.setState ({ loading: true });
 
     // Send the user text to our api to get a response
-    axios.get('http://127.0.0.1:3001/cetus', {
+    axios.get('http://localhost:3001/cetus', {
       params: {
         userText: this.state.userInput
       }
